6aca451c25bf0b2e3accc7ccf5ecdc2b7844ed82
manishapaudel <manishapaudel093@gmail.com>
2024-11-26 19:32:08 +0545

    updating homepage

--- movie_project/js/script.js
+++ movie_project/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { title: "Chhaka panja: Endgame", image: "assets/images/chhakapanja.jpg" },
         { title: "pashupatiprasad", image: "assets/images/pashupatiprasad.jpg" },
         { title: "Sarangi", image: "assets/images/sarangi.jpg" },
+        { title: "pashupatiprasad", image: "assets/images/pashupatiprasad.jpg" },
     ];
 
     const movieList = document.querySelector('.movie-list');
