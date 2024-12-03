6b1973a9d6d3fba03309842b8b1887dc77320f7a
priyanka-bca <priyanku060@gmail.com>
2024-12-03 20:40:05 +0545

    scripting edit

--- movie_ticket/config.php
+++ movie_ticket/config.php
@@ -21,6 +21,24 @@ $sql = "CREATE TABLE Login (
 
 if (mysqli_query($conn, $sql)) {
     echo "Table Login created successfully";
+
+    // Insert 5 usernames and passwords with password hashing
+    $users = [
+        ['username' => 'riya', 'password' => password_hash('password1', PASSWORD_DEFAULT)],
+        ['username' => 'priyanka', 'password' => password_hash('password2', PASSWORD_DEFAULT)],
+        ['username' => 'ram', 'password' => password_hash('password3', PASSWORD_DEFAULT)],
+        ['username' => 'shyam', 'password' => password_hash('password4', PASSWORD_DEFAULT)],
+        ['username' => 'hari', 'password' => password_hash('password5', PASSWORD_DEFAULT)]
+    ];
+
+    foreach ($users as $user) {
+        $sql = "INSERT INTO Login (username, password) VALUES (?, ?)";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "ss", $user['username'], $user['password']);
+        mysqli_stmt_execute($stmt);
+    }
+
+    echo "Users inserted successfully";
 } else {
     echo "Error creating table: " . mysqli_error($conn);
 }
