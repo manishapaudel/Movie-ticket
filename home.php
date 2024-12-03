cfe354c7e97e5d36db68d3211c2a0ca00b2789a7
priyanka-bca <priyanku060@gmail.com>
2024-12-03 16:44:00 +0545

    photo

--- home.php
+++ home.php
@@ -13,16 +13,11 @@
             <ul>
                 <li><a href="">Home</a></li>
                 <li><a href="">About Us</a></li>
-                <li><a href="">Contact Us</a></li>
-                <li><a href="">Hello</a></li>
-
+                <li><a href="">Booking</a></li>
                 <li><a href="Login.php">Login</a></li>
                 </ul>>
         </div>
-        <div class="cuurentmovies">
-            
-
-        </div>
+        
     </div>
 </body>
 </html>
