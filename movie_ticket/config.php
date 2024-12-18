6ef124bd47da64bba5b80a98bf647762c859f821
priyanka-bca <priyanku060@gmail.com>
2024-12-18 19:37:15 +0545

    adding colums in db

--- movie_ticket/config.php
+++ movie_ticket/config.php
@@ -4,12 +4,34 @@ $username = "root";
 $password = "";
 $dbname = "mydb";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "ALTER TABLE users ADD COLUMN is_admin BOOLEAN DEFAULT 0";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+// try {
+//   $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+//   // set the PDO error mode to exception
+//   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   // echo "Connected successfully";
+// } catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
+
 ?>
+
+
+
+
+
